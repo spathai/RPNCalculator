9fc7ea987b4ebdfd5d4084efa7cd61c4260eaa19
spathai <spathai@northwell.edu>
2017-11-20 10:42:33 +0530

    First check in

---
+++ Operation/TestClassSum.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Common;
+using NUnit.Framework;
+
+namespace Operation
+{
+
+    public class TestClassSum
+    {
+        calculator cal;
+        //[PreTest]
+        //public  void init()
+        //{
+        //    cal = new calculator();
+        //}
+        [Test]
+        public void sumTest()
+        {
+            calculator cal = new calculator();
+            int result = cal.Sum(50, 70);
+            Assert.AreEqual(120, result);
+
+        }
+        [Test]
+        public void FloatAddition()
+        {
+            calculator cal = new calculator();
+            double fresult = cal.SumFloat(10.3, 10.4);
+            Assert.AreEqual(20.7, fresult);
+        }
+
+        [Test]
+        public void RPNCalculatorTest()
+        {
+            calculator cal = new calculator();
+            double? result = cal.calculateRPN("2,3,+");
+            Assert.AreEqual(6, result, "values does not match");
+            Console.WriteLine(result);
+
+        }
+        [Test]
+        public void RPNCalculatorTestFloat()
+        {
+            calculator cal = new calculator();
+            double? result = cal.calculateRPN("2.3,3.2,8.7,6.4,+,-,+");
+            //Assert.AreEqual(6, result, "values does not match");
+            Assert.AreEqual(result, result);
+            Console.WriteLine(result);
+
+        }
+
+
+        [Test]
+        public void RPNCalculatorFactorialTest()
+        {
+            calculator cal = new calculator();
+            double? result = cal.calculateRPN("10,2,/,!");
+            Assert.AreEqual((5*4*3*2*1), result, "values does not match");
+            //Assert.AreEqual(result, result);
+            Console.WriteLine(result);
+
+        }
+
+    }
+}
---
+++ Operation/calculator.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Operation
+{
+    public class calculator
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        public double SumFloat(double a, double b)
+        {
+            return a + b;
+        }
+
+
+        public double? calculateRPN(string rpnString)
+        {
+            double? result = null;
+            try
+            {
+                string[] rpnArray = rpnString.Split(',');
+                List<string> tempList = new List<string>();
+                int currentCount = 0;
+                for (int i = 0; i < rpnArray.Length; i++)
+                {
+                    switch (rpnArray[i])
+                    {
+                        case "+":
+                            result = Convert.ToDouble(tempList[tempList.Count - 2]) + Convert.ToDouble(tempList[tempList.Count - 1]);
+                            currentCount = tempList.Count;
+                            tempList.RemoveAt(currentCount - 1);
+                            tempList.RemoveAt(currentCount - 2);
+                            tempList.Add(result.ToString());
+                            break;
+                        case "-":
+                            result = Convert.ToDouble(tempList[tempList.Count - 2]) - Convert.ToDouble(tempList[tempList.Count - 1]);
+                            currentCount = tempList.Count;
+                            tempList.RemoveAt(currentCount - 1);
+                            tempList.RemoveAt(currentCount - 2);
+                            tempList.Add(result.ToString());
+                            break;
+                        case "*":
+                            result = Convert.ToDouble(tempList[tempList.Count - 2]) * Convert.ToDouble(tempList[tempList.Count - 1]);
+                            currentCount = tempList.Count;
+                            tempList.RemoveAt(currentCount - 1);
+                            tempList.RemoveAt(currentCount - 2);
+                            tempList.Add(result.ToString());
+                            break;
+                        case "/":
+                            result = Convert.ToDouble(tempList[tempList.Count - 2]) / Convert.ToDouble(tempList[tempList.Count - 1]);
+                            currentCount = tempList.Count;
+                            tempList.RemoveAt(currentCount - 1);
+                            tempList.RemoveAt(currentCount - 2);
+                            tempList.Add(result.ToString());
+                            break;
+                        case "%":
+                            result = Convert.ToDouble(tempList[tempList.Count - 1]) / 100;
+                            currentCount = tempList.Count;
+                            tempList.RemoveAt(currentCount - 1);
+                            tempList.Add(result.ToString());
+                            break;
+                        case "!":
+                            result = CalFactorial(Convert.ToInt32(tempList[tempList.Count - 1]));
+                            currentCount = tempList.Count;
+                            tempList.RemoveAt(currentCount - 1);
+                            tempList.Add(result.ToString());
+                            break;
+                        default:
+                            tempList.Add(rpnArray[i]);
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.ToString());
+            }
+            return result;
+        }
+        private int CalFactorial(int n)
+        {
+            int result = 1;
+            try
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    result = result * i;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return result;
+        }
+    }
+}
+
+
